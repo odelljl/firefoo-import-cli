240cc4d63b5cf0f854adac155cc13967e2876cfc
Jeff Odell <jeff@odellmail.com>
2024-07-29 15:02:26 -0400

    Add CLI functionality and update dependencies
    Implemented Commander.js to handle command-line options and figlet for terminal art. Updated package.json to include new dependencies and added a build:watch script. Set up FireFoo CLI for input file handling and verbose output.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Hello, TypeScript with Node.js!');
+#! /usr/bin/env node
+
+import { Command } from 'commander';
+import figlet from 'figlet';
+import * as fs from 'fs';
+
+const program = new Command();
+program
+  .version('1.0.0') // -V by default, short circuits any further processing
+  .description(
+    'CLI for importing files into FireBase as exported from the FireFoo utility',
+  )
+  .option('-v, --verbose', 'show verbose output information')
+  .option('-i, --input <value>', 'path to the input file')
+  .parse(process.argv);
+
+const options = program.opts();
+
+console.log(figlet.textSync('FireFoo CLI Import/Export'));
+
+console.log(options);
+
+// show help if no options presented
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
+
+// check for input and execute
+const inputFilePath = options.input;
+if (inputFilePath) {
+  if (options.verbose) {
+    console.log('Verbose output enabled');
+  }
+  if (!fs.existsSync(inputFilePath)) process.exit(1);
+
+  // start import
+}
+
+// async function importFile(path: string) {}
