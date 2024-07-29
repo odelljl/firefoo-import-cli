e3b7eab8e4a06afa650191388b11a7c62388d625
Jeff Odell <jeff@odellmail.com>
2024-07-29 15:59:25 -0400

    Refactor import functionality and add error handling
    Removed verbose option, streamlined import logic, and added error handling to ensure file existence before attempting import. Additionally, introduced helper functions for logging import status and created a new test JSON file for validation.

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,65 @@ program
   .description(
     'CLI for importing files into FireBase as exported from the FireFoo utility',
   )
-  .option('-v, --verbose', 'show verbose output information')
   .option('-i, --input <value>', 'path to the input file')
   .parse(process.argv);
 
 const options = program.opts();
 
-console.log(figlet.textSync('FireFoo CLI Import/Export'));
-
-console.log(options);
+console.log(figlet.textSync('FireFoo Import CLI'));
 
 // show help if no options presented
 if (!process.argv.slice(2).length) {
   program.outputHelp();
 }
 
-// check for input and execute
 const inputFilePath = options.input;
 if (inputFilePath) {
-  if (options.verbose) {
-    console.log('Verbose output enabled');
+  // if (options.verbose) {
+  //   console.log('Verbose output enabled');
+  // }
+
+  // check for input and execute
+  if (!fs.existsSync(inputFilePath)) {
+    logError(`File ${inputFilePath} does not exist`);
+    logImportFailed();
+    process.exit(1);
   }
-  if (!fs.existsSync(inputFilePath)) process.exit(1);
 
   // start import
+  importFile(inputFilePath)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .then((_) => logImportCompleted())
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .catch((_) => {
+      logImportFailed();
+      process.exit(1);
+    });
 }
 
-// async function importFile(path: string) {}
+// FUNCTIONS
+
+async function importFile(path: string) {
+  try {
+    const fileContent = await fs.promises.readFile(path, 'utf-8');
+    console.log(fileContent);
+  } catch (error) {
+    logImportFailed();
+  }
+}
+
+function logError(err: string) {
+  console.log('\x1b[31m', err);
+}
+
+function logSuccess(err: string) {
+  console.log('\x1b[32m', err);
+}
+
+function logImportFailed() {
+  logError('\nImport failed.');
+}
+
+function logImportCompleted() {
+  logSuccess('\nImport complete.');
+}
